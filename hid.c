91079f88d73033d39abfc9e865b91603740ac5b6
Pavel Hübner <pavel.hubner@bigclown.com>
2020-03-02 10:10:53 +0100

    Implement enumeration - wip

--- hid.c
+++ hid.c
@@ -9,18 +9,52 @@
 
 int hid_open(hid_device_t *device, unsigned short vendor_id, unsigned short product_id)
 {
+    CFMutableDictionaryRef matching = CFDictionaryCreateMutable(kCFAllocatorDefault, kIOHIDOptionsTypeNone, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+    if (matching == NULL)
+        return -1;
+
+    CFNumberRef v = CFNumberCreate(kCFAllocatorDefault, kCFNumberShortType, &vendor_id);
+    CFDictionarySetValue(matching, CFSTR(kIOHIDVendorIDKey), v);
+    CFRelease(v);
+
+    CFNumberRef p = CFNumberCreate(kCFAllocatorDefault, kCFNumberShortType, &product_id);
+    CFDictionarySetValue(matching, CFSTR(kIOHIDProductIDKey), p);
+    CFRelease(p);
+
+    IOHIDManagerRef manager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+    IOHIDManagerSetDeviceMatching(manager, NULL); // TODO it crashes without this call
+
+    //if (IOHIDManagerOpen(manager, kIOHIDOptionsTypeNone) != kIOReturnSuccess)
+        //return -2;
+
+    IOHIDManagerSetDeviceMatching(manager, matching);
+    CFRelease(matching);
+
+    CFSetRef devices = IOHIDManagerCopyDevices(manager);
+    CFIndex count = CFSetGetCount(devices);
+
+    printf("Device count: %ld\n", (long) CFSetGetCount(devices));
+    /*
+
+    IOHIDManagerClose(manager, kIOHIDOptionsTypeNone);
+	CFRelease(manager);
+
+    CFRelease(devices);
+    */
+
 	io_registry_entry_t entry = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS10@14600000/FT260@14600000/IOUSBHostInterface@0/AppleUserUSBHostHIDDevice");
 
 	if (entry == MACH_PORT_NULL)
-        return -1;
+        return -3;
 
 	*device = IOHIDDeviceCreate(kCFAllocatorDefault, entry);
 
 	if (*device == NULL)
-        return -2;
+        return -4;
 
 	if (IOHIDDeviceOpen(*device, kIOHIDOptionsTypeSeizeDevice) != kIOReturnSuccess)
-        return -3;
+        return -5;
 
     return 0;
 }
