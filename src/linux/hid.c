a83323e284d0a232fe12f43be0e3b1a6814e8073
Karel Blavka <karel.blavka@hardwario.com>
2020-08-20 09:09:11 +0200

    Update hid for linux

--- src/linux/hid.c
+++ src/linux/hid.c
@@ -4,13 +4,65 @@
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <libudev.h>
+#include <util.h>
 
 int hid_open(hid_device_t *device, int vendor_id, int product_id)
 {
-    (void) vendor_id;
-    (void) product_id;
+    struct udev *udev;
+    udev = udev_new();
 
-    *device = open("/dev/hidraw0", O_RDWR);
+    if (udev == NULL)
+        die("Call `udev_new` failed");
+
+    char *hid_path = NULL;
+
+    struct udev_enumerate *enumerate;
+    struct udev_list_entry *entry, *devices_hid;
+    struct udev_device *hid_dev;
+    struct udev_device *usb_dev;
+    const char *str;
+
+    enumerate = udev_enumerate_new(udev);
+    udev_enumerate_add_match_subsystem(enumerate, "hidraw");
+    udev_enumerate_scan_devices(enumerate);
+    devices_hid = udev_enumerate_get_list_entry(enumerate);
+
+    udev_list_entry_foreach(entry, devices_hid)
+    {
+        str = (char *) udev_list_entry_get_name(entry);
+        hid_dev = udev_device_new_from_syspath(udev, str);
+
+        usb_dev = udev_device_get_parent_with_subsystem_devtype(hid_dev, "usb", "usb_device");
+
+        str = udev_device_get_sysattr_value(usb_dev, "idVendor");
+
+        if ((uint16_t) ((str) ? strtol(str, NULL, 16) : -1) != vendor_id)
+        {
+            udev_device_unref(hid_dev);
+            continue;
+        }
+
+        str = udev_device_get_sysattr_value(usb_dev, "idProduct");
+
+        if ((uint16_t) ((str) ? strtol(str, NULL, 16) : -1) != product_id)
+        {
+            udev_device_unref(hid_dev);
+            continue;
+        }
+
+        hid_path = strdup(udev_device_get_devnode(hid_dev));
+        udev_device_unref(hid_dev);
+        break;
+    }
+
+    udev_enumerate_unref(enumerate);
+    udev_unref(udev);
+
+    if (hid_path == NULL)
+        return -1;
+
+    *device = open(hid_path, O_RDWR);
 
     if (*device == -1)
         return -1;
